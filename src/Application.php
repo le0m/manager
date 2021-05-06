70b5dfd14506d0e4230af8dd6935fd3f53b2fad1
stefanorosanelli <stefano.rosanelli@gmail.com>
2021-05-06 11:39:16 +0200

    feat: updatet Application.php

--- src/Application.php
+++ src/Application.php
@@ -14,12 +14,13 @@ namespace App;
 
 use App\Middleware\ProjectMiddleware;
 use BEdita\I18n\Middleware\I18nMiddleware;
-use BEdita\WebTools\BaseApplication;
 use Cake\Core\Configure;
 use Cake\Core\Configure\Engine\PhpConfig;
 use Cake\Error\Middleware\ErrorHandlerMiddleware;
-use Cake\Http\MiddlewareQueue;
+use Cake\Http\BaseApplication;
+use Cake\Http\Middleware\BodyParserMiddleware;
 use Cake\Http\Middleware\CsrfProtectionMiddleware;
+use Cake\Http\MiddlewareQueue;
 use Cake\Routing\Middleware\AssetMiddleware;
 use Cake\Routing\Middleware\RoutingMiddleware;
 
@@ -33,7 +34,7 @@ class Application extends BaseApplication
      *
      * @var array
      */
-    const PLUGIN_DEFAULTS = [
+    protected const PLUGIN_DEFAULTS = [
         'debugOnly' => false,
         'autoload' => false,
         'bootstrap' => true,
@@ -42,12 +43,35 @@ class Application extends BaseApplication
     ];
 
     /**
-     * {@inheritDoc}
+     * @inheritDoc
      */
     public function bootstrap(): void
     {
         parent::bootstrap();
-        $this->addPlugin('BEdita/WebTools', ['bootstrap' => true]);
+        if (PHP_SAPI === 'cli') {
+            $this->bootstrapCli();
+        }
+        /*
+         * Only try to load DebugKit in development mode
+         * Debug Kit should not be installed on a production system
+         */
+        if (Configure::read('debug')) {
+            $this->addOptionalPlugin('DebugKit');
+        }
+
+        // Load 'BEdita/WebTools' and other plugins here
+        $this->addPlugin('BEdita/WebTools');
+        // To activate I18nMiddleware use $this->addPlugin('BEdita/I18n', ['middleware' => true]);
+        $this->addPlugin('BEdita/I18n');
+
+        /*
+         * Uncomment to setup AssetsRevisions with an appropriate strategy.
+         *
+         * @see https://github.com/bedita/web-tools#load-assets-with-assetrevisions
+         */
+        // \BEdita\WebTools\Utility\AssetsRevisions::setStrategy(
+        //     new \BEdita\WebTools\Utility\Asset\Strategy\EntrypointsStrategy()
+        // );
     }
 
     /**
@@ -55,8 +79,7 @@ class Application extends BaseApplication
      */
     protected function bootstrapCli(): void
     {
-        parent::bootstrapCli();
-        $this->addPlugin('BEdita/I18n');
+        $this->addOptionalPlugin('Bake');
     }
 
     /**
@@ -70,7 +93,7 @@ class Application extends BaseApplication
     {
         $plugins = (array)Configure::read('Plugins');
         foreach ($plugins as $plugin => $options) {
-            $options = array_merge(self::PLUGIN_DEFAULTS, $options);
+            $options = array_merge(static::PLUGIN_DEFAULTS, $options);
             if (!$options['debugOnly'] || ($options['debugOnly'] && Configure::read('debug'))) {
                 $this->addPlugin($plugin, $options);
                 $this->plugins->get($plugin)->bootstrap($this);
@@ -79,14 +102,14 @@ class Application extends BaseApplication
     }
 
     /**
-     * {@inheritDoc}
+     * @inheritDoc
      */
-    public function middleware($middlewareQueue): MiddlewareQueue
+    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
     {
         $middlewareQueue
             // Catch any exceptions in the lower layers,
             // and make an error page/response
-            ->add(new ErrorHandlerMiddleware(null, Configure::read('Error')))
+            ->add(new ErrorHandlerMiddleware(Configure::read('Error')))
 
             // Load current project configuration if `multiproject` instance
             // Manager plugins will also be loaded here via `loadPluginsFromConfig()`
@@ -104,9 +127,20 @@ class Application extends BaseApplication
             ]))
 
             // Add routing middleware.
+            // If you have a large number of routes connected, turning on routes
+            // caching in production could improve performance. For that when
+            // creating the middleware instance specify the cache config name by
+            // using it's second constructor argument:
+            // `new RoutingMiddleware($this, '_cake_routes_')`
             ->add(new RoutingMiddleware($this))
 
-            // Csrf Middleware
+            // Parse various types of encoded request bodies so that they are
+            // available as array through $request->getData()
+            // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
+            ->add(new BodyParserMiddleware())
+
+            // Cross Site Request Forgery (CSRF) Protection Middleware
+            // https://book.cakephp.org/4/en/controllers/middleware.html#cross-site-request-forgery-csrf-middleware
             ->add($this->csrfMiddleware());
 
         return $middlewareQueue;
@@ -115,12 +149,12 @@ class Application extends BaseApplication
     /**
      * Get internal Csrf Middleware
      *
-     * @return CsrfProtectionMiddleware
+     * @return \Cake\Http\Middleware\CsrfProtectionMiddleware
      */
     protected function csrfMiddleware(): CsrfProtectionMiddleware
     {
         // Csrf Middleware
-        $csrf = new CsrfProtectionMiddleware();
+        $csrf = new CsrfProtectionMiddleware(['httponly' => true]);
         // Token check will be skipped when callback returns `true`.
         $csrf->skipCheckCallback(function ($request) {
             $actions = (array)Configure::read(sprintf('CsrfExceptions.%s', $request->getParam('controller')));
